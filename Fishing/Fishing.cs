0db9e690388cb4fc9825e8fe9b5166c7ba072b27
Diogo Pinela <diogoid7400@gmail.com>
2025-04-20 13:16:57 +0100

    Make fishing spots into real IC locations

--- Fishing/Fishing.cs
+++ Fishing/Fishing.cs
@@ -1,10 +1,8 @@
 using MAPI = Modding;
 using static Modding.Utils.UnityExtensions;
-using CG = System.Collections.Generic;
 using UE = UnityEngine;
-using USM = UnityEngine.SceneManagement;
+using CG = System.Collections.Generic;
 using IC = ItemChanger;
-using TMP = TMPro;
 
 namespace Fishing;
 
@@ -17,73 +15,30 @@ public class Fishing : MAPI.Mod
         (IC.SceneNames.Ruins_Bathhouse, SitTemplateName)
     };
 
-    private UE.GameObject? fishingSpotPrefab = null;
-
     public override void Initialize(CG.Dictionary<string, CG.Dictionary<string, UE.GameObject>> preloads)
     {
-        fishingSpotPrefab = preloads[IC.SceneNames.Ruins_Bathhouse][SitTemplateName];
+        FishingLocation.FishingSpotPrefab = preloads[IC.SceneNames.Ruins_Bathhouse][SitTemplateName];
 
-        IC.Events.OnSceneChange += PlaceFishingSpots;
+        IC.Finder.DefineCustomLocation(FishingLocation.LakeOfUnn);
+
+        On.UIManager.StartNewGame += PlaceFishingSpots;
     }
 
-    private void PlaceFishingSpots(USM.Scene scene)
+    private void PlaceFishingSpots(On.UIManager.orig_StartNewGame orig, UIManager self, bool permaDeath, bool bossRush)
     {
-        if (scene.name != IC.SceneNames.Fungus1_26)
+        try
         {
-            return;
+            IC.ItemChangerMod.CreateSettingsProfile(overwrite: false, createDefaultModules: false);
+            var p = IC.Finder.GetLocation("Fishing Spot-Lake of Unn")!
+                .Wrap()
+                .Add(IC.Finder.GetItem("Rancid_Egg")!);
+            IC.ItemChangerMod.AddPlacements(new CG.List<IC.AbstractPlacement> {p});
         }
-        var obj = UE.Object.Instantiate(fishingSpotPrefab!);
-        obj.transform.position = new UE.Vector3(58.1f, 15.6f, obj.transform.position.z);
-        var fsm = obj.LocateMyFSM("Shop Region");
-        fsm.GetFsmFloat("Move To X").Value = 58.1f;
-        var arrow = fsm.GetFsmGameObject("Prompt");
-        fsm.GetState("Init").AppendAction(() =>
+        catch (System.Exception err)
         {
-            var prompts = FindImmediateChild(arrow.Value, "Labels");
-            var sit = FindImmediateChild(prompts, "Sit");
-            UE.Object.Destroy(sit.GetComponent<SetTextMeshProGameText>());
-            var text = sit.GetComponent<TMP.TextMeshPro>();
-            text.text = "FISH";
-        });
-        fsm.GetState("Sit").AppendAction(() =>
-        {
-            var loc = new IC.Locations.CoordinateLocation()
-            {
-                name = "Fishing Spot #1",
-                sceneName = IC.SceneNames.Fungus1_26,
-                flingType = IC.FlingType.Everywhere,
-                forceShiny = true,
-                x = 0,
-                y = 0,
-                elevation = 0,
-            };
-            loc.AddTag<IC.Tags.ShinyFlingTag>().fling = IC.ShinyFling.Right;
-            var p = new IC.Placements.MutablePlacement("Fishing Spot #1")
-            {
-                Location = loc,
-                containerType = IC.Container.Shiny,
-            };
-            p.Items.Add(IC.Finder.GetItem("Rancid_Egg")!);
-            var ci = new IC.ContainerInfo(IC.Container.Shiny, p, IC.FlingType.Everywhere);
-            var s = IC.Util.ShinyUtility.MakeNewMultiItemShiny(ci);
-            s.transform.position = new UE.Vector3(56.1f, 12.6f, s.transform.position.z);
-            s.SetActive(true);
-        });
-        obj.SetActive(true);
-    }
-
-    private UE.GameObject FindImmediateChild(UE.GameObject parent, string childName)
-    {
-        var transform = parent.transform;
-        for (var i = 0; i < transform.childCount; i++)
-        {
-            var c = transform.GetChild(i).gameObject;
-            if (c.name == childName)
-            {
-                return c;
-            }
+            LogError($"error initializing fishing spots: {err}");
         }
-        throw new System.InvalidOperationException($"GO {parent.name} has no child named {childName}");
+        orig(self, permaDeath, bossRush);
     }
 
     public override string GetVersion() => "1.0";
---
+++ Fishing/FishingLocation.cs
@@ -0,0 +1,89 @@
+using IC = ItemChanger;
+using UE = UnityEngine;
+using USM = UnityEngine.SceneManagement;
+using TMP = TMPro;
+
+namespace Fishing;
+
+internal class FishingLocation : IC.Locations.AutoLocation
+{
+    public static UE.GameObject? FishingSpotPrefab;
+
+    public FishingLocation(IC.ShinyFling flingDirection)
+    {
+        AddTag<IC.Tags.ShinyFlingTag>().fling = flingDirection;
+    }
+
+    public float MarkerX;
+    public float MarkerY;
+    public float ShinySourceX;
+    public float ShinySourceY;
+
+    protected override void OnLoad()
+    {
+        IC.Events.AddSceneChangeEdit(sceneName!, PlaceFishingSpot);
+    }
+
+    protected override void OnUnload()
+    {
+        IC.Events.RemoveSceneChangeEdit(sceneName!, PlaceFishingSpot);
+    }
+
+    private void PlaceFishingSpot(USM.Scene scene)
+    {
+        var obj = UE.Object.Instantiate(FishingSpotPrefab!);
+        obj.transform.position = new UE.Vector3(MarkerX, MarkerY, obj.transform.position.z);
+        obj.SetActive(true);
+        var fsm = obj.LocateMyFSM("Shop Region");
+        fsm.GetFsmFloat("Move To X").Value = MarkerX;
+        var arrow = fsm.GetFsmGameObject("Prompt");
+        fsm.GetState("Init").AppendAction(() =>
+        {
+            var prompts = FindImmediateChild(arrow.Value, "Labels");
+            var sit = FindImmediateChild(prompts, "Sit");
+            UE.Object.Destroy(sit.GetComponent<SetTextMeshProGameText>());
+            var text = sit.GetComponent<TMP.TextMeshPro>();
+            text.text = "FISH";
+        });
+        if (Placement.AllObtained())
+        {
+            return;
+        }
+        fsm.GetState("Sit").AppendAction(() =>
+        {
+            var ci = new IC.ContainerInfo(IC.Container.Shiny, Placement, IC.FlingType.Everywhere);
+            var s = IC.Util.ShinyUtility.MakeNewMultiItemShiny(ci);
+            s.transform.position = new UE.Vector3(ShinySourceX, ShinySourceY, s.transform.position.z);
+            s.SetActive(true);
+        });
+    }
+
+    private static UE.GameObject FindImmediateChild(UE.GameObject parent, string childName)
+    {
+        var transform = parent.transform;
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            var c = transform.GetChild(i).gameObject;
+            if (c.name == childName)
+            {
+                return c;
+            }
+        }
+        throw new System.InvalidOperationException($"GO {parent.name} has no child named {childName}");
+    }
+
+    public override IC.AbstractPlacement Wrap() => new IC.Placements.AutoPlacement(name)
+    {
+        Location = this,
+    };
+
+    public static readonly FishingLocation LakeOfUnn = new(IC.ShinyFling.Right)
+    {
+        name = "Fishing Spot-Lake of Unn",
+        sceneName = IC.SceneNames.Fungus1_26,
+        MarkerX = 58.1f,
+        MarkerY = 15.6f,
+        ShinySourceX = 56.1f,
+        ShinySourceY = 11.6f,
+    };
+}
